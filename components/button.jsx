93e97d83430cd2d09d0058b166bf87821cb1eebc
LopezDev <aragonlopezjuan87@gmail.com>
2022-05-19 19:07:27 +0200

    feat: create footer componet

--- components/button.jsx
+++ components/button.jsx
@@ -1,6 +1,6 @@
 export const Button = ({ children, icon }) => {
     return (
-        <button className='flex items-center gap-4 px-4 py-2 bg-blue-500 text-white rounded-md'>
+        <button className='shadow-lg shadow-blue-500/50 flex items-center gap-4 px-4 py-2 bg-blue-500 text-white rounded-md'>
             {icon}
             {children}
         </button>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { Nav } from '../components/nav'
 import { Hero } from '../components/hero'
+import { Footer } from '../components/footer'
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <main className='h-screen'  >
         <Nav />
         <Hero />
+        <Footer />
       </main>
     </div>
 
